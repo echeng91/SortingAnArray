a54dada3e0882e2793d32958254e18f9a7880f87
echeng91 <cheng.eric612@gmail.com>
2016-03-04 11:00:22 -0500

    sorting an array

---
+++ src/SortingValues.java
@@ -0,0 +1,37 @@
+public class SortingValues
+{
+	public static void main( String[] args )
+	{
+		int[] arr = { 45, 87, 39, 32, 93, 86, 12, 44, 75, 50 };
+		int temp;
+
+		// Display the original (unsorted values)
+		System.out.print("before: ");
+		for ( int i=0; i<arr.length; i++ )
+			System.out.print( arr[i] + " " );
+		System.out.println();
+
+		// Swap the values around to put them ascending order.
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			for (int j = i; j < arr.length; j++)
+			{
+				if (arr[j] < arr[i])
+				{
+					// swap the values in two slots
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+
+		// Display the values again, now (hopefully) sorted.
+		System.out.print("after : ");
+		for ( int i=0; i<arr.length; i++ )
+			System.out.print( arr[i] + " " );
+		System.out.println();
+	}
+}
